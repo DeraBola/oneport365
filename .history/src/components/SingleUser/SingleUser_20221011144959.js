0489d7a1f85fd647fba89cc4591937a5b57f056c
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-10-14 10:19:41 +0100

    commit

---
+++ .history/src/components/SingleUser/SingleUser_20221011144959.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+import TopNavbar from "../TopNavbar/TopNavbar";
+import "./SingleUser.css";
+
+function SingleUser() {
+  const {id} = useParams( );
+
+  const [users, setusers] = useState([])
+   
+  useEffect(() => { 
+
+  const getUserRequest = `https://demo3522726.mockable.io/get_single_customer/123456789?/${id}`
+       fetch(getUserRequest)
+      .then((response) => response.json())
+      .then((json) => {console.log(json);
+        setusers(json)});
+  }, [ id ] );
+
+  return (
+    <>
+      <TopNavbar title="Shipments" />
+      <section className="customers_container">
+        <div className="singleuser_container">
+          <div className="singleuser_top">
+            <Link to="/customers">
+              <div className="arrow">
+                <BiArrowBack />
+              </div>
+            </Link>
+            {
+             users && (
+              <div className="card" key={users.id} >
+                <div className="image">
+                  
+                  <img src={users.Avatar} alt="" />
+                  <div className="card_content">
+                  <h3>
+                    {users.first_name}
+                    {users.last_name}
+                  </h3>
+                  <h4>{users.email}</h4>
+                  <h4>{users.phone}</h4>
+                </div>
+                </div>
+                <div className="card_edit">Edit</div>
+              </div>
+            )}
+          </div>
+          <div className="singleuser_middle"></div>
+          <div className="singleuser_bottom"></div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default SingleUser;
