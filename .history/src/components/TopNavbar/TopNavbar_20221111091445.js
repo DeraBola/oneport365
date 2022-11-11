bb740c287efb4d2c7249b448b498568c0c3074f7
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-11-11 11:10:31 +0100

    commit

---
+++ .history/src/components/TopNavbar/TopNavbar_20221111091445.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { BsBell } from "react-icons/bs";
+import "./TopNavbar.css";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import { BsFillCaretDownFill } from "react-icons/bs";
+
+const TopNavbar = ({ title }) => {
+  const [dropdown, setdropdown] = useState(false);
+
+  return (
+    <>
+      <nav className="navtop_container">
+        <div className="title-body" >
+          <p className="p__Inter">{title}</p>
+        </div>
+
+        <div className="top_container" onMouseLeave={() => setdropdown(false)}  >
+          <div className="top_content">
+           .t <BsBell className="top_content_bell" />
+            <span>2</span>
+          </div>
+
+          <div className="dropdown_container">
+            <div
+              className="dropdown_industries"
+              onMouseEnter={() => setdropdown(true)}
+            >
+              <p>Industries < BsFillCaretDownFill /></p> 
+            </div>
+          </div>
+          {dropdown && <Dropdown />}
+        </div>
+      </nav>
+      
+    </>
+  );
+};
+
+export default TopNavbar;
---
+++ .history/src/components/UploadDocument/UploadDocument_20221111073435.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import UploadedImg from "../../assets/img/Invoices.png";
+import DropZone from "../../components/DropZone/DropZone";
+import { IoDocumentText } from "react-icons/io5";
+import { BiTrash } from "react-icons/bi";
+import "./UploadDocument.css";
+
+function UploadDocument() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const [uploadedfile, setuploadedFile] = useState([]);
+
+  const removeFile = (file) => () => {
+    const newFiles = [...uploadedfile];
+    newFiles.splice(newFiles.indexOf(file), 1);
+    setuploadedFile(newFiles);
+  };
+
+  return (
+    <div className="loaded_document">
+      <div className="loaded_document_top">
+        <div className="loaded_document_text">
+          <p>Uploaded Document</p>
+        </div>
+        <div className="loaded_request">
+          <div className="request_doc">
+            
+            <p>Request Document</p>
+          </div>
+          <div
+            className="uploaded_doc"
+            onClick={() => {
+              setModalOpen(true);
+            }}
+          >
+            <p>Upload Document</p> .   <img src={UploadedImg} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="modal_container">
+        {modalOpen && (
+          <DropZone
+            setOpenModal={setModalOpen}
+            setFile={setFile}
+            File={file}
+            setuploadedFile={setuploadedFile}
+            uploadedFile={uploadedfile}
+          />
+        )}
+
+        {uploadedfile.map((uploadedfiles, id) => (
+          <>
+            <div className="dropzone_content" key={id}>
+              <div className="dropzone_file">
+                <div className="file_icon">
+                  <IoDocumentText />
+                </div>
+                <p className=" file_text ">
+                  {uploadedfiles ? uploadedfiles[0].name : ""}
+                </p>
+                <div className="dropzone_img">
+                  <img src={URL.createObjectURL(uploadedfiles[0])} alt="" />
+                </div>
+              </div>
+              <div className="dropzone_delete" onClick={removeFile(file)}>
+                <BiTrash />
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default UploadDocument;
